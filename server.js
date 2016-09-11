cd45c9f6f01e293caf070e10c40f2ed2a1af4fe1
Amy Briggs <amylbd@gmail.com>
2016-09-11 15:53:38 -0600

    finished

--- server.js
+++ server.js
@@ -16,151 +16,156 @@ app.disable('x-powered-by');
 
 app.use(morgan('short'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/pets', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
-    var pets = JSON.parse(petsJSON);
+        var pets = JSON.parse(petsJSON);
 
-    res.send(pets);
-  })
+        res.send(pets);
+    })
 })
 
 app.get('/pets/:id', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
-    var id = Number.parseInt(req.params.id);
-    var pets = JSON.parse(petsJSON);
+        var id = Number.parseInt(req.params.id);
+        var pets = JSON.parse(petsJSON);
 
-    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-      return res.sendStatus(404);
-    }
+        if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+            return res.sendStatus(404);
+        }
 
-    // res.set('Content-Type', 'text/plain');
-    res.send(pets[id]);
-  });
+        // res.set('Content-Type', 'text/plain');
+        res.send(pets[id]);
+    });
 });
 
 app.post('/pets', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
 
-// I need to define the variables that make up a new pet: age, kind, and
-// name
-// var age = parseInt(req.body.age)
-// var kind = req.body.kind
-// var name = req.body.name
-var pets = JSON.parse(petsJSON);
-// var petsJSON = req.body.pet;
-//
-    var pet = {
-      age: parseInt(req.body.age),
-      kind: req.body.kind,
-      name: req.body.name,
-    }
 
-    if (!pet) {
-      return res.sendStatus(400);
-    }
+        var pets = JSON.parse(petsJSON);
 
-    pets.push(pet)
-    console.log("new pet added!");
+        var pet = {
+            age: parseInt(req.body.age),
+            kind: req.body.kind,
+            name: req.body.name,
+        }
 
-    var newPetsJSON = JSON.stringify(pets);
+        if (!pet) {
+            return res.sendStatus(400);
+        } else if (pet.kind === "" || pet.name === "") {
+            return res.sendStatus(400);
+        } else if (isNaN(pet.age)) {
+            return res.sendStatus(400);
+        }
 
-    fs.writeFile(petsPath, newPetsJSON, function(writeErr) {
-      if (writeErr) {
-        console.error(writeErr.stack);
-        return res.sendStatus(500);
-      }
+        pets.push(pet)
+        console.log("new pet added!");
 
-      res.set('Content-Type', 'text/plain');
-      res.send(pet);
+        var newPetsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, newPetsJSON, function(writeErr) {
+            if (writeErr) {
+                console.error(writeErr.stack);
+                return res.sendStatus(500);
+            }
+
+            res.set('Content-Type', 'text/plain');
+            res.send(pet);
+        });
     });
-  });
 });
 
 app.put('/pets/:id', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
-    if (readErr) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+    fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
+        if (readErr) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
-    var id = Number.parseInt(req.params.id);
-    var pets = JSON.parse(petsJSON);
+        var id = Number.parseInt(req.params.id);
+        var pets = JSON.parse(petsJSON);
 
-    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-      return res.sendStatus(404);
-    }
+        if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+            return res.sendStatus(404);
+        }
 
-    var pet = {
-      age: parseInt(req.body.age),
-      kind: req.body.kind,
-      name: req.body.name,
-    }
+        var pet = {
+            age: parseInt(req.body.age),
+            kind: req.body.kind,
+            name: req.body.name,
+        }
 
-    if (!pet) {
-      return res.sendStatus(400);
-    }
+        if (!pet) {
+            return res.sendStatus(400);
+        } else if (pet.kind === "" || pet.name === "") {
+            return res.sendStatus(400);
+        } else if (isNaN(pet.age)) {
+            return res.sendStatus(400);
+        }
 
-    pets[id] = pet;
+        pets[id] = pet;
 
-    var newPetsJSON = JSON.stringify(pets);
+        var newPetsJSON = JSON.stringify(pets);
 
-    fs.writeFile(petsPath, newPetsJSON, function(err) {
-      if (err) {
-        console.error(err.stack);
-        return res.sendStatus(500);
-      }
+        fs.writeFile(petsPath, newPetsJSON, function(err) {
+            if (err) {
+                console.error(err.stack);
+                return res.sendStatus(500);
+            }
 
-      res.set('Content-Type', 'text/plain');
-      res.send(pet);
+            res.set('Content-Type', 'text/plain');
+            res.send(pet);
+        });
     });
-  });
 });
 
 app.delete('/pets/:id', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
-    var id = Number.parseInt(req.params.id);
-    var pets = JSON.parse(petsJSON);
+        var id = Number.parseInt(req.params.id);
+        var pets = JSON.parse(petsJSON);
 
-    if (id < 0 || id >= pets.length || Number.isNaN(id) ) {
-      return res.sendStatus(404);
-    }
+        if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+            return res.sendStatus(404);
+        }
 
 
-    var pet = pets.splice(id, 1)[0];
-    var newPetsJSON = JSON.stringify(pets);
+        var pet = pets.splice(id, 1)[0];
+        var newPetsJSON = JSON.stringify(pets);
 
-    fs.writeFile(petsPath, newPetsJSON, function(writeErr) {
-      if (writeErr) {
-        console.error(writeErr.stack);
-        return res.sendStatus(500);
-      }
+        fs.writeFile(petsPath, newPetsJSON, function(writeErr) {
+            if (writeErr) {
+                console.error(writeErr.stack);
+                return res.sendStatus(500);
+            }
 
-      res.set('Content-Type', 'text/plain');
-      res.send(pet);
+            res.set('Content-Type', 'text/plain');
+            res.send(pet);
+        });
     });
-  });
 });
 
 // app.use(function(req, res) {
@@ -168,5 +173,5 @@ app.delete('/pets/:id', function(req, res) {
 // });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+    console.log('Listening on port', port);
 });
